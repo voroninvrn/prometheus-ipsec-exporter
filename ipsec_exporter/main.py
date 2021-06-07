907b47dd60960b69a89fba48825a117f2b423a7f
root <root@p306i43vnr.os.loc>
2021-06-07 12:41:43 +0300

    fix script

--- ipsec_exporter/main.py
+++ ipsec_exporter/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import subprocess
 import glob
@@ -13,33 +13,30 @@ class IpsecExporter:
         self.app = Flask(__name__)
         self.connections = self.get_connections()
         if not self.connections:
-            print("There's no files in /etc/ipsec.d/")
+            print("There's no files in /etc/ipsec.conf/")
             exit(1)
-        self.gauge = Gauge("ipsec_tunnel_status",
-                           "Output from the check_ipsec script",
-                           ["connection_name"])
+        self.gauge = Gauge("ipsec_tunnel_status", "Output from the check_ipsec script", ["connection_name"])
+        self.rtt = Gauge("ipsec_tunnel_rtt", "Output from the check_ipsec script", ["connection_name"])
         self.run_webserver()
 
     def get_connections(self):
         "Get ipsec's connection name with it's initialized prometheus gauge."
-        ipsec_conf_files = glob.glob("/etc/ipsec.d/*.conf")
+        ipsec_conf_files = glob.glob('/etc/ipsec.conf')
         connections = []
 
         for ipsec_conf_file in ipsec_conf_files:
-            # Read all files on /etc/ipsec.d/*.conf
+            # Read all files on /etc/ipsec.conf/*.conf
             f = open(ipsec_conf_file)
             lines = f.readlines()
             f.close()
-
             # Get all lines that match with the next regex
             r = re.compile("^conn")
-
             # Assume there"s only one conn per file and change some chars
-            connection = list(
-                filter(r.match, lines)
-            )[0].split(" ")[1]
-            connection = str(connection).replace("\n", "")
-            connections.append(connection)
+            connection = list(filter(r.match, lines))[1:]
+            for conn in connection[:]:
+                connection = str(conn).replace("\n", "")
+                connection = str(connection).replace("conn", "")
+                connections.append(connection)
 
         return connections
 
@@ -47,6 +44,7 @@ class IpsecExporter:
         "Main method to serve the metrics."
         connections = self.connections
         gauge = self.gauge
+        rtt = self.rtt
 
         @self.app.route("/metrics")
         def metrics():
@@ -55,16 +53,20 @@ class IpsecExporter:
             it gets, it executes the 'check_ipsec' command.
             """
             for conn in connections:
-                ipsec_process = subprocess.run(["check_ipsec", conn],
-                                               stdout=subprocess.PIPE)
+                ipsec_process = subprocess.run(["check_ipsec", conn], stdout=subprocess.PIPE)
                 ipsec_process = float(ipsec_process.stdout)
                 gauge.labels(conn).set(ipsec_process)
 
+    #        metrics = generate_latest()
+
+     #       for conn in connections:
+                ipsec_process_rtt = subprocess.run(['check_ipsec', 'rtt', conn], stdout=subprocess.PIPE)
+                ipsec_process_rtt = float(ipsec_process_rtt.stdout)
+                rtt.labels(conn).set(ipsec_process_rtt)
+
             metrics = generate_latest()
 
-            return Response(metrics,
-                            mimetype='text/plain',
-                            content_type='text/plain; charset=utf-8')
+            return Response(metrics, mimetype='text/plain', content_type='text/plain; charset=utf-8')
 
     def run_webserver(self):
         "Start the web application."
